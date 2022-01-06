77d5c638c89df790934e9cc9d5352b7eb1235557
kikimanojlovic <97203975+kikimanojlovic@users.noreply.github.com>
2022-01-06 02:28:46 +0100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,37 @@
+Last login: Wed Jan  5 20:41:38 on ttys001
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+(base) Kristinas-MacBook-Pro:~ kiki$ 
+(base) Kristinas-MacBook-Pro:~ kiki$ python
+Python 3.8.8 (default, Apr 13 2021, 12:59:45) 
+[Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> git clone https://github.com/gavagnin/DSF_Week2.git
+  File "<stdin>", line 1
+    git clone https://github.com/gavagnin/DSF_Week2.git
+        ^
+SyntaxError: invalid syntax
+>>> git clone hello_world.py
+  File "<stdin>", line 1
+    git clone hello_world.py
+        ^
+SyntaxError: invalid syntax
+>>> a=3
+>>> b=4
+>>> a+b
+7
+>>> import random
+>>> x= random.randint(1,6)
+>>> print(x)
+6
+>>> print(x)
+6
+>>> x= random.randint(1,6)
+>>> print(x)
+2
+>>> 
+>>> print("hello World")
+hello World
+>>> 
